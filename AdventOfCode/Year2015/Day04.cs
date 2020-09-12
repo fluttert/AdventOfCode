c049967441fef80dd312eb4ed16f501f841a49fd
Ernst Fluttert <e.fluttert@gmail.com>
2020-09-12 20:03:29 +0200

    Year 2015 Day 04

---
+++ AdventOfCode/Year2015/Day04.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Text;
+
+namespace AdventOfCode.Year2015
+{
+    public class Day04 : IAoC
+    {
+        public string input = "yzbqklnj";
+
+        public string SolvePart1(string input)
+        {
+            int number = 0;
+            bool leadingZerosFound = false;
+            while (!leadingZerosFound)
+            {
+                if (CalculateMD5(input + number).StartsWith("00000")){
+                    break;
+                }
+                    number++;
+            }
+            return number.ToString();
+        }
+
+        public string SolvePart2(string input)
+        {
+            int number = 0;
+            bool leadingZerosFound = false;
+            while (!leadingZerosFound)
+            {
+                if (CalculateMD5(input + number).StartsWith("000000"))
+                {
+                    break;
+                }
+                number++;
+            }
+            return number.ToString();
+        }
+
+        // Calculate MD5 hashes, based on https://docs.microsoft.com/en-us/troubleshoot/dotnet/csharp/compute-hash-values
+        public string CalculateMD5(string input)
+        {
+            using (System.Security.Cryptography.MD5 md5 = System.Security.Cryptography.MD5.Create())
+            {
+                byte[] inputBytes = System.Text.Encoding.ASCII.GetBytes(input);
+                byte[] hashBytes = md5.ComputeHash(inputBytes);
+
+                // Convert the byte array to hexadecimal string
+                StringBuilder sb = new StringBuilder();
+                for (int i = 0; i < hashBytes.Length; i++)
+                {
+                    sb.Append(hashBytes[i].ToString("X2"));
+                }
+                return sb.ToString();
+            }
+        }
+    }
+}
---
+++ AdventOfCodeTests/Year2015/Day04Tests.cs
@@ -0,0 +1,23 @@
+using AdventOfCode.Year2015;
+using Xunit;
+
+namespace AdventOfCodeTests
+{
+    public class Day04Tests
+    {
+        private readonly Day04 day = new Day04();
+
+        [Fact]
+        public void Part01()
+        {
+            Assert.Equal("609043", day.SolvePart1("abcdef"));
+            Assert.Equal("1048970", day.SolvePart1("pqrstuv"));
+        }
+
+        [Fact]
+        public void Part02()
+        {
+           
+        }
+    }
+}
