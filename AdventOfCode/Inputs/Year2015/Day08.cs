2ca87a688e37108c30926e5100de03c0a827dc51
Ernst Fluttert <e.fluttert@gmail.com>
2020-09-15 21:23:20 +0200

    clean up

--- AdventOfCode/Inputs/Year2015/Day08.cs
+++ AdventOfCode/Inputs/Year2015/Day08.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace AdventOfCode.Inputs.Year2015
+﻿namespace AdventOfCode.Inputs.Year2015
 {
     public class Day08
     {
