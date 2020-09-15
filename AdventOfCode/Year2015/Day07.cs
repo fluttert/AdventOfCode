fced938e06cac6be8c0cc2447c057fcb27e689b7
Ernst Fluttert <e.fluttert@gmail.com>
2020-09-15 20:24:41 +0200

    deduplicated day 07
    cuz it was ugly

--- AdventOfCode/Year2015/Day07.cs
+++ AdventOfCode/Year2015/Day07.cs
@@ -27,127 +27,15 @@ namespace AdventOfCode.Year2015
 
         public string SolvePart1(string input)
         {
-            string[] lines = input.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries);
-
-            // A wire has unsigned short aka unsigned 16 bit integer, range 0-65635
-            // more types? see https://docs.microsoft.com/en-us/dotnet/csharp/language-reference/builtin-types/integral-numeric-types
-            // also the built-in conversion are a must read https://docs.microsoft.com/en-us/dotnet/csharp/language-reference/builtin-types/numeric-conversions
-
-            var wiresValues = new Dictionary<string, ushort>();
-            var nodes = new Dictionary<string, Node>();
-
-            // add all wires (nodes)
-            foreach (string line in lines)
-            {
-                // have a key for each node
-                string nodeId = (line.Split(new char[] { ' ' })).Last();
-                nodes.Add(nodeId, new Node(nodeId, line));
-            }
-
-            var stack = new Stack<string>();
-            stack.Push("a");
-            while (stack.Count > 0)
-            {
-                Node wire = nodes[stack.Peek()];
-                if (wire.signalReady)
-                {
-                    if (!wiresValues.ContainsKey(wire.id))
-                    {
-                        wiresValues.Add(wire.id, wire.signal);
-                    }
-                    stack.Pop();
-                    continue;
-                }
-
-                // else process the node
-                string[] parts = wire.circuit.Split(new char[] { '-', '>', ' ' }, StringSplitOptions.RemoveEmptyEntries);
-
-                // Assign
-                if (parts.Length == 2)
-                {
-                    if (char.IsDigit(parts[0][0]))
-                    {
-                        wire.signal = ushort.Parse(parts[0]);
-                        wire.signalReady = true;
-                        continue;
-                    }
-                    if (wiresValues.ContainsKey(parts[0]))
-                    {
-                        wire.signal = wiresValues[parts[0]];
-                        wire.signalReady = true;
-                        continue;
-                    }
-                    stack.Push(parts[0]);
-                    continue;
-                }
-                // NOT
-                if (parts[0] == "NOT")
-                {
-                    if (wiresValues.ContainsKey(parts[1]))
-                    {
-                        wire.signal = bitwiseNot(wiresValues[parts[1]]);
-                        wire.signalReady = true;
-                    }
-                    else { stack.Push(parts[1]); }
-                    continue;
-                }
-
-                // AND
-                if (parts[1] == "AND") {
-                    if (char.IsDigit(parts[0][0]) && wiresValues.ContainsKey(parts[2])) {
-                        wire.signal = bitwiseAnd(ushort.Parse(parts[0]), wiresValues[parts[2]]);
-                        wire.signalReady = true;
-                        continue;
-                    }
-
-                    if (wiresValues.ContainsKey(parts[0]) && wiresValues.ContainsKey(parts[2])) {
-                        wire.signal = bitwiseAnd(wiresValues[parts[0]], wiresValues[parts[2]]);
-                        wire.signalReady = true;
-                    }
-                    if (!char.IsDigit(parts[0][0]) && !wiresValues.ContainsKey(parts[0])) { stack.Push(parts[0]); }
-                    if (!wiresValues.ContainsKey(parts[2])) { stack.Push(parts[2]); }
-                    continue;
-                }
-
-                // OR
-                if (parts[1] == "OR")
-                {
-                    if (wiresValues.ContainsKey(parts[0]) && wiresValues.ContainsKey(parts[2]))
-                    {
-                        wire.signal = bitwiseOr(wiresValues[parts[0]], wiresValues[parts[2]]);
-                        wire.signalReady = true;
-                    }
-                    if (!wiresValues.ContainsKey(parts[0])) { stack.Push(parts[0]); }
-                    if (!wiresValues.ContainsKey(parts[2])) { stack.Push(parts[2]); }
-                    continue;
-                }
-
-                // SHIFT LEFT
-                if (parts[1] == "LSHIFT") {
-                    if (wiresValues.ContainsKey(parts[0]))
-                    {
-                        wire.signal = bitwiseLeftShift(wiresValues[parts[0]], int.Parse(parts[2]));
-                        wire.signalReady = true;
-                    }
-                    else { stack.Push(parts[0]); }
-                    continue;
-                }
-                // SHIFT LEFT
-                if (parts[1] == "RSHIFT")
-                {
-                    if (wiresValues.ContainsKey(parts[0]))
-                    {
-                        wire.signal = bitwiseRightshift(wiresValues[parts[0]], int.Parse(parts[2]));
-                        wire.signalReady = true;
-                    }
-                    else { stack.Push(parts[0]); }
-                }
-            }
-            return wiresValues["a"].ToString();
+            return BitWiseLogicGates(input);
         }
 
         public string SolvePart2(string input)
         {
+            return BitWiseLogicGates(input, 956);
+        }
+
+        public string BitWiseLogicGates(string input, ushort inputB = 0) {
             string[] lines = input.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries);
             var wiresValues = new Dictionary<string, ushort>();
             var nodes = new Dictionary<string, Node>();
@@ -161,8 +49,9 @@ namespace AdventOfCode.Year2015
             }
 
             // add node B
-            wiresValues.Add("b", 956);
-
+            if(inputB > 0) { 
+            wiresValues.Add("b", inputB);
+            }
 
             var stack = new Stack<string>();
             stack.Push("a");
@@ -268,6 +157,7 @@ namespace AdventOfCode.Year2015
                 }
             }
             return wiresValues["a"].ToString();
+
         }
 
         public ushort bitwiseAnd(ushort val1, ushort val2)
