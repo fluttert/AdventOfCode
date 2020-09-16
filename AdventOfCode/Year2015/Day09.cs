886a176aabb071ac8ad76f937f8ff1c2e71ab5b0
Ernst Fluttert <e.fluttert@gmail.com>
2020-09-16 08:02:18 +0200

    refactor day 9

--- AdventOfCode/Year2015/Day09.cs
+++ AdventOfCode/Year2015/Day09.cs
@@ -10,43 +10,21 @@ namespace AdventOfCode.Year2015
 
         public string SolvePart1(string input)
         {
-            string[] lines = input.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries);
-            int shortestDistance = int.MaxValue;
-            
-            // create pairs 
-            var pairs = new Dictionary<string, Dictionary<string, int>>();
-            var uniqueplaces = new HashSet<string>();
-            foreach (string line in lines) {
-                string[] parts = line.Split(' ', StringSplitOptions.RemoveEmptyEntries);
-                if (!pairs.ContainsKey(parts[0])) { pairs.Add(parts[0], new Dictionary<string, int>()); }
-                if (!pairs.ContainsKey(parts[2])) { pairs.Add(parts[2], new Dictionary<string, int>()); }
-                pairs[parts[0]].Add(parts[2], int.Parse(parts[4]));
-                pairs[parts[2]].Add(parts[0], int.Parse(parts[4]));
-                uniqueplaces.Add(parts[0]);
-                uniqueplaces.Add(parts[2]);
-            }
-
-            var locations = uniqueplaces.ToArray();
-            // permutate!
-            var permutations = GetPermutations<string>(locations, locations.Length);
-            foreach (var permutation in permutations) {
-                List<string> cur = permutation.ToList<string>();
-                int distance = 0;
-                for (int i = 1; i < cur.Count; i++) {
-                    distance += pairs[cur[i - 1]][cur[i]];
-                }
-                if (distance < shortestDistance) { shortestDistance = distance; }
-            }
-
-            return shortestDistance.ToString() ;
+            return CalculateDistances(input, true);
         }
 
         public string SolvePart2(string input)
         {
+            return CalculateDistances(input, false);
+        }
+
+        internal string CalculateDistances(string input, bool shortest = true)
+        {
             string[] lines = input.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries);
+            int shortestDistance = int.MaxValue;
             int longestDistance = int.MinValue;
 
-            // create pairs 
+            // create pairs
             var pairs = new Dictionary<string, Dictionary<string, int>>();
             var uniqueplaces = new HashSet<string>();
             foreach (string line in lines)
@@ -71,17 +49,17 @@ namespace AdventOfCode.Year2015
                 {
                     distance += pairs[cur[i - 1]][cur[i]];
                 }
+                if (distance < shortestDistance) { shortestDistance = distance; }
                 if (distance > longestDistance) { longestDistance = distance; }
             }
 
-            return longestDistance.ToString();
+            return shortest ? shortestDistance.ToString() : longestDistance.ToString();
         }
 
         public string GetInput()
         {
             return new Inputs.Year2015.Day09().Input;
         }
-
 
         // thank you https://stackoverflow.com/questions/756055/listing-all-permutations-of-a-string-integer
         internal static IEnumerable<IEnumerable<T>>
