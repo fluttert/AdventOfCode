ebf070ef9bc1549b9e7d0595fa069c0faa81bbec
Ernst Fluttert <e.fluttert@gmail.com>
2020-09-19 18:31:11 +0200

    2015 Day 12, JSON without encoding
    Nuts assignment, no using the json, just simple string manipulation

---
+++ AdventOfCode/Year2015/Day12.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections.Generic;
+using System.Text;
+
+namespace AdventOfCode.Year2015
+{
+    public class Day12 : IAoC
+    {
+        // Challenge can be found on https://adventofcode.com/2015/day/12
+
+        public string SolvePart1(string input)
+        {
+            // try to solve without parsing to JSON
+            int total = 0;
+            List<char> possibleNumber = new List<char>();
+            for (int i = 0; i < input.Length; i++)
+            {
+                // detect any valid characters for numbers
+                if (input[i] == '-' || (input[i] >= '0' && input[i] <= '9'))
+                {
+                    possibleNumber.Add(input[i]);
+                    continue;
+                }
+
+                // nothing to parse
+                if (possibleNumber.Count == 0) { continue; }
+
+                // else parse to a number
+                total += int.Parse(new string(possibleNumber.ToArray()));
+
+                // clear the chars
+                possibleNumber.Clear();
+            }
+            return total.ToString(); ;
+        }
+
+        public string SolvePart2(string input)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            bool inputFiltered = false;
+            while (!inputFiltered)
+            {
+                int loc = input.IndexOf(":\"red\"");
+                if (loc == -1) { inputFiltered = true; break; }
+
+                // search curly braces
+                int openingBracket = -1;
+                int closingBracket = -1;
+
+                int extra = 0;
+                for (int i = loc; i < input.Length; i++)
+                {
+                    if (input[i] == '{') { extra++; }
+                    if (input[i] == '}')
+                    {
+                        if (extra > 0) { extra--; continue; }
+                        closingBracket = i; break;
+                    }
+                }
+
+                extra = 0;
+                for (int i = loc - 1; i >= 0; i--)
+                {
+
+                    if (input[i] == '}') { extra++; }
+                    if (input[i] == '{') {
+                        if (extra > 0) { extra--; continue; }
+                        openingBracket = i; break; 
+                    }
+                }
+
+                sb.Append(input.Substring(0, openingBracket));
+                sb.Append(input.Substring(closingBracket + 1));
+                input = sb.ToString();
+                sb.Clear();
+            }
+            return SolvePart1(input);
+        }
+
+        public string GetInput()
+        {
+            return new Inputs.Year2015.Day12().Input;
+        }
+    }
+}
---
+++ AdventOfCodeTests/Year2015/Day12Tests.cs
@@ -0,0 +1,33 @@
+using AdventOfCode.Year2015;
+using Xunit;
+
+namespace AdventOfCodeTests
+{
+    public class Day12Tests
+    {
+        private readonly Day12 day = new Day12();
+
+        [Fact]
+        public void Part01()
+        {
+            Assert.Equal("6", day.SolvePart1("[1,2,3]"));
+            Assert.Equal("6", day.SolvePart1("{\"a\":2,\"b\":4}"));
+            Assert.Equal("3", day.SolvePart1("[[[3]]]"));
+            Assert.Equal("3", day.SolvePart1("{\"a\":{\"b\":4},\"c\":-1}"));
+            Assert.Equal("3", day.SolvePart1("{\"a\":{\"b\":4},\"c\":-1}"));
+            Assert.Equal("0", day.SolvePart1("{\"a\":[-1,1]}"));
+            Assert.Equal("0", day.SolvePart1("[-1,{\"a\":1}]"));
+            Assert.Equal("0", day.SolvePart1("[]"));
+            Assert.Equal("0", day.SolvePart1("{}"));
+        }
+
+        [Fact]
+        public void Part02()
+        {
+            Assert.Equal("6", day.SolvePart2("[1,\"red\",5]"));
+            Assert.Equal("4", day.SolvePart2("[1,{\"c\":\"red\",\"b\":2},3]"));
+            Assert.Equal("0", day.SolvePart2("{\"d\":\"red\",\"e\":[1,2,3,4],\"f\":5}"));
+            Assert.Equal("6", day.SolvePart2("[1,2,3]"));
+        }
+    }
+}
