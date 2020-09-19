c9b8566fedbe6eb574364a9a7f88733d2a8bd6ee
Ernst Fluttert <e.fluttert@gmail.com>
2020-09-19 21:40:44 +0200

    Year 2015 Day 14 solution
    race time

---
+++ AdventOfCode/Year2015/Day14.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AdventOfCode.Year2015
+{
+    public class Day14 : IAoC
+    {
+        // Challenge can be found on https://adventofcode.com/2015/day/14
+
+        public string SolvePart1(string input)
+        {
+            return SolvePart1(input, 2503);
+        }
+
+        public string SolvePart1(string input, int seconds)
+        {
+            string[] lines = input.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries);
+
+            int maxDistance = int.MinValue;
+            foreach (string line in lines)
+            {
+                string[] parts = line.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                int speed = int.Parse(parts[3]);
+                int travelTime = int.Parse(parts[6]);
+                int resttime = int.Parse(parts[^2]);
+                int period = travelTime + resttime;
+
+                int distance = 0;
+                for (int i = 0; i <= seconds; i++)
+                {
+                    int secondInPeriod = ((period + i) % period);
+                    distance += secondInPeriod < travelTime ? speed : 0;
+                }
+
+                if (distance > maxDistance) { maxDistance = distance; }
+            }
+
+            return maxDistance.ToString(); ;
+        }
+
+        public string SolvePart2(string input)
+        {
+            return SolvePart2(input, 2503);
+        }
+
+        public string SolvePart2(string input, int seconds)
+        {
+            string[] lines = input.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries);
+
+            // index of reindeer -> speed, traveltime, period
+            //var reindeers = new Dictionary<string, Tuple<int,int, int>>();
+            var reindeers = new Dictionary<string, (int speed, int traveltime, int period)>();
+
+            var reindeerDistance = new Dictionary<string, int>();
+            var reindeerPoints = new Dictionary<string, int>();
+
+            // set-up the race
+            foreach (string line in lines)
+            {
+                string[] parts = line.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                string reindeer = parts[0];
+                int speed = int.Parse(parts[3]);
+                int travelTime = int.Parse(parts[6]);
+                int resttime = int.Parse(parts[^2]);
+                int period = travelTime + resttime;
+
+                reindeers.Add(reindeer, (speed, travelTime, period));
+                reindeerDistance.Add(reindeer, 0);
+                reindeerPoints.Add(reindeer, 0);
+            }
+
+            // RACE!
+            for (int i = 0; i <= seconds; i++)
+            {
+                int farthest = 0;
+
+                foreach (var r in reindeers)
+                {
+                    int distance = ((r.Value.period + i) % r.Value.period) < r.Value.traveltime ? r.Value.speed : 0;
+                    reindeerDistance[r.Key] += distance;
+                    if (farthest < reindeerDistance[r.Key]) { farthest = reindeerDistance[r.Key]; }
+                }
+
+                foreach (var r in reindeerDistance)
+                {
+                    if (r.Value == farthest)
+                    {
+                        reindeerPoints[r.Key]++;
+                    }
+                }
+            }
+
+            // determine winner!
+            int maxPoints = 0;
+            foreach (var r in reindeerPoints)
+            {
+                if (r.Value > maxPoints) { maxPoints = r.Value; }
+            }
+
+            return maxPoints.ToString(); ;
+        }
+
+        public string GetInput()
+        {
+            return new Inputs.Year2015.Day14().Input;
+        }
+    }
+}
---
+++ AdventOfCodeTests/Year2015/Day14Tests.cs
@@ -0,0 +1,29 @@
+using AdventOfCode.Year2015;
+using Xunit;
+
+namespace AdventOfCodeTests
+{
+    public class Day14Tests
+    {
+        private readonly Day14 day = new Day14();
+
+        [Fact]
+        public void Part01()
+        {
+            string input = @"Comet can fly 14 km/s for 10 seconds, but then must rest for 127 seconds.
+Dancer can fly 16 km/s for 11 seconds, but then must rest for 162 seconds.";
+            
+            Assert.Equal("1120", day.SolvePart1(input,1000));
+           
+        }
+
+        [Fact]
+        public void Part02()
+        {
+            string input = @"Comet can fly 14 km/s for 10 seconds, but then must rest for 127 seconds.
+Dancer can fly 16 km/s for 11 seconds, but then must rest for 162 seconds.";
+
+            Assert.Equal("689", day.SolvePart2(input, 1000));
+        }
+    }
+}
