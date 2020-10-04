88fec4b99dae57736aea61a2b60e20d7539f507f
Ernst Fluttert <e.fluttert@gmail.com>
2020-10-04 11:11:06 +0200

    DeepCopy aka Duplicate lists

---
+++ AdventOfCode/Utils/Utils.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+
+namespace AdventOfCode.Utils
+{
+    public class Utils
+    {
+        /// <summary>
+        /// Duplicate a list, with no reference to the original (aka DeepCopy)
+        /// </summary>
+        /// <typeparam name="T">Type of List</typeparam>
+        /// <param name="original">original list</param>
+        /// <returns>duplicate of the original list</returns>
+        public static List<T> Duplicate<T>(List<T> original)
+        {
+            var output = new List<T>();
+            for (int i = 0; i < original.Count; i++)
+            {
+                output.Add(original[i]);
+            }
+            return output;
+        }
+    }
+}
