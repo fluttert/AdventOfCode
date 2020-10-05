816855a64a469c129b1a2705f404b7d3e4ec58bb
Ernst Fluttert <e.fluttert@gmail.com>
2020-10-05 21:01:18 +0200

    Update Structure

--- AdventOfCode/Program.cs
+++ AdventOfCode/Program.cs
@@ -8,37 +8,41 @@ namespace AdventOfCode
         private static void Main()
         {
             var aoc = new AdventOfCode();
+            aoc.Solved();
 
             // Change this to the day you want to run
-            aoc.Solve(new Year2015.Day25());
-            //aoc.Solve(new Year2015.Day24());
-            //aoc.Solve(new Year2015.Day23());
-            //aoc.Solve(new Year2015.Day22());
-            //aoc.Solve(new Year2015.Day21());
-            //aoc.Solve(new Year2015.Day20());
-            //aoc.Solve(new Year2015.Day19());
-            //aoc.Solve(new Year2015.Day18());
-            //aoc.Solve(new Year2015.Day17());
-            //aoc.Solve(new Year2015.Day16());
-            //aoc.Solve(new Year2015.Day15());
-            //aoc.Solve(new Year2015.Day14());
-            //aoc.Solve(new Year2015.Day13());
-            //aoc.Solve(new Year2015.Day12());
-            //aoc.Solve(new Year2015.Day11());
-            //aoc.Solve(new Year2015.Day10());
-            //aoc.Solve(new Year2015.Day09());
-            //aoc.Solve(new Year2015.Day08());
-            //aoc.Solve(new Year2015.Day07());
-            //aoc.Solve(new Year2015.Day06());
-            //aoc.Solve(new Year2015.Day05());
-            //aoc.Solve(new Year2015.Day04()); // slow: 18370ms
-            //aoc.Solve(new Year2015.Day03());
-            //aoc.Solve(new Year2015.Day02());
-            //aoc.Solve(new Year2015.Day01());
-
-            // timing
 
             Console.ReadLine();
+        }
+
+        private void Solved()
+        {
+            var aoc = new AdventOfCode();
+            aoc.Solve(new Year2015.Day25());
+            aoc.Solve(new Year2015.Day24());
+            aoc.Solve(new Year2015.Day23());
+            aoc.Solve(new Year2015.Day22());
+            aoc.Solve(new Year2015.Day21());
+            aoc.Solve(new Year2015.Day20());
+            aoc.Solve(new Year2015.Day19());
+            aoc.Solve(new Year2015.Day18());
+            aoc.Solve(new Year2015.Day17());
+            aoc.Solve(new Year2015.Day16());
+            aoc.Solve(new Year2015.Day15());
+            aoc.Solve(new Year2015.Day14());
+            aoc.Solve(new Year2015.Day13());
+            aoc.Solve(new Year2015.Day12());
+            aoc.Solve(new Year2015.Day11());
+            aoc.Solve(new Year2015.Day10());
+            aoc.Solve(new Year2015.Day09());
+            aoc.Solve(new Year2015.Day08());
+            aoc.Solve(new Year2015.Day07());
+            aoc.Solve(new Year2015.Day06());
+            aoc.Solve(new Year2015.Day05());
+            //aoc.Solve(new Year2015.Day04()); // slow: 18370ms
+            aoc.Solve(new Year2015.Day03());
+            aoc.Solve(new Year2015.Day02());
+            aoc.Solve(new Year2015.Day01());
         }
 
         public void Solve(IAoC aoc)
