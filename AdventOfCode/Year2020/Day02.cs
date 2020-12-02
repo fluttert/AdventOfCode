ab703d3e8d45a08a7e6524dbb5d64e92b47c1b41
Ernst Fluttert <e.fluttert@gmail.com>
2020-12-02 08:22:52 +0100

    Update Day02.cs
    typo

--- AdventOfCode/Year2020/Day02.cs
+++ AdventOfCode/Year2020/Day02.cs
@@ -4,7 +4,7 @@ namespace AdventOfCode.Year2020
 {
     public class Day02 : IAoC
     {
-        // Puzzle can be found on: https://adventofcode.com/2020/day/1
+        // Puzzle can be found on: https://adventofcode.com/2020/day/2
 
         public string SolvePart1(string input)
         {
