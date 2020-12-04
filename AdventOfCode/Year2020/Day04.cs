f7e79ce183c910064b4074d3c8aca01d3bb60f2f
Fluttert <e.fluttert@gmail.com>
2020-12-04 13:24:58 +0100

    improved readibility of Day 04

--- AdventOfCode/Year2020/Day04.cs
+++ AdventOfCode/Year2020/Day04.cs
@@ -9,30 +9,33 @@ namespace AdventOfCode.Year2020
 
         public string SolvePart1(string input)
         {
-            // read per line
+            // read per line, and do NOT remove empty entries (e.g. empty lines)
             string[] lines = input.Split(Environment.NewLine);
 
-            //byr(Birth Year)       = 0
-            //iyr(Issue Year)       = 1
-            //eyr(Expiration Year)  = 2
-            //hgt(Height)           = 3
-            //hcl(Hair Color)       = 4
-            //ecl(Eye Color)        = 5 
-            //pid(Passport ID)      = 6
-            //cid(Country ID) -> optional
+            // Have an boolean array per passport, to check the required fields (and ignore everything else)
+            // byr(Birth Year)       = 0
+            // iyr(Issue Year)       = 1
+            // eyr(Expiration Year)  = 2
+            // hgt(Height)           = 3
+            // hcl(Hair Color)       = 4
+            // ecl(Eye Color)        = 5
+            // pid(Passport ID)      = 6
+            // cid(Country ID) -> optional, so ignored
+            // any other field will be ignored
 
-            bool[] passport= new bool[7];
-            int validPassports = 0;
+            bool[] passport = new bool[7]; // will be initialized as false (to all)
+            int totalValidPassports = 0;         
             for (int i = 0; i < lines.Length; i++)
             {
-                // reset on newline
-                if(lines[i] == ""){
+                // reset the passport at every empty line
+                if (lines[i] == "")
+                {
                     passport = new bool[7];
                 }
 
-                // else parse the pieces, and check the EVEN pieces
+                // else parse the pieces, and check the field-labels (which is in this case all the even pieces)
                 string[] pieces = lines[i].Split(new char[] { ' ', ':' }, StringSplitOptions.RemoveEmptyEntries);
-                for (int j = 0; j < pieces.Length; j+=2)
+                for (int j = 0; j < pieces.Length; j += 2)
                 {
                     if (pieces[j] == "byr") { passport[0] = true; }
                     if (pieces[j] == "iyr") { passport[1] = true; }
@@ -47,15 +50,20 @@ namespace AdventOfCode.Year2020
                 bool validPass = true;
                 for (int j = 0; j < passport.Length; j++)
                 {
+                    // not complete? then continue
                     if (!passport[j]) { validPass = false; break; }
                 }
-                if (validPass) { 
-                    validPassports++;
-                    passport = new bool[7];
+
+                // have a valid passport (all 7 required fields)
+                if (validPass)
+                {
+                    totalValidPassports++;
+                    // and ofc reset the passport, not to count something double 
+                    passport = new bool[7]; 
                 }
             }
 
-            return validPassports.ToString();
+            return totalValidPassports.ToString();
         }
 
         public string SolvePart2(string input)
@@ -63,19 +71,21 @@ namespace AdventOfCode.Year2020
             // read per line
             string[] lines = input.Split(Environment.NewLine);
 
-            //byr(Birth Year)       = 0 - four digits; at least 1920 and at most 2002
-            //iyr(Issue Year)       = 1 - four digits; at least 2010 and at most 2020
-            //eyr(Expiration Year)  = 2 - four digits; at least 2020 and at most 2030
-            //hgt(Height)           = 3 - a number followed by either cm or in: If cm, the number must be at least 150 and at most 193. If in, the number must be at least 59 and at most 76
-            //hcl(Hair Color)       = 4 - a # followed by exactly six characters 0-9 or a-f
-            //ecl(Eye Color)        = 5 - amb blu brn gry grn hzl oth
-            //pid(Passport ID)      = 6 - a nine-digit number, including leading zeroes
-            //cid(Country ID) -> optional
+            // Have an boolean array per passport, to check the required fields (and ignore everything else)
+            // byr(Birth Year)       = 0 - four digits; at least 1920 and at most 2002
+            // iyr(Issue Year)       = 1 - four digits; at least 2010 and at most 2020
+            // eyr(Expiration Year)  = 2 - four digits; at least 2020 and at most 2030
+            // hgt(Height)           = 3 - a number followed by either cm or in: If cm, the number must be at least 150 and at most 193. If in, the number must be at least 59 and at most 76
+            // hcl(Hair Color)       = 4 - a # followed by exactly six characters 0-9 or a-f
+            // ecl(Eye Color)        = 5 - amb blu brn gry grn hzl oth
+            // pid(Passport ID)      = 6 - a nine-digit number, including leading zeroes
+            // cid(Country ID) -> optional
+            // ignore everything else
 
             bool[] passport = new bool[7];
-            int validPassports = 0;
+            int totalValidPassports = 0;
             var eyeColors = new HashSet<string>() { "amb", "blu", "brn", "gry", "grn", "hzl", "oth" };
-            
+
             for (int i = 0; i < lines.Length; i++)
             {
                 // reset on newline
@@ -91,10 +101,10 @@ namespace AdventOfCode.Year2020
                     if (pieces[j] == "byr") { int year = int.Parse(pieces[j + 1]); if (1920 <= year && year <= 2002) { passport[0] = true; } }
                     if (pieces[j] == "iyr") { int issueYear = int.Parse(pieces[j + 1]); if (2010 <= issueYear && issueYear <= 2020) { passport[1] = true; } }
                     if (pieces[j] == "eyr") { int expireYear = int.Parse(pieces[j + 1]); if (2020 <= expireYear && expireYear <= 2030) { passport[2] = true; } }
-                    if (pieces[j] == "hgt") { if(validHeight(pieces[j+1])){ passport[3] = true; } }
-                    if (pieces[j] == "hcl") { if ( validHairColor(pieces[j+1]) ) { passport[4] = true; } }
-                    if (pieces[j] == "ecl") { if ( eyeColors.Contains(pieces[j+1]) ) { passport[5] = true; } }
-                    if (pieces[j] == "pid") { if ( validPassportNumber(pieces[j+1])) { passport[6] = true; } }
+                    if (pieces[j] == "hgt") { if (ValidHeight(pieces[j + 1])) { passport[3] = true; } }
+                    if (pieces[j] == "hcl") { if (ValidHairColor(pieces[j + 1])) { passport[4] = true; } }
+                    if (pieces[j] == "ecl") { if (eyeColors.Contains(pieces[j + 1])) { passport[5] = true; } }
+                    if (pieces[j] == "pid") { if (ValidPassportNumber(pieces[j + 1])) { passport[6] = true; } }
                 }
 
                 // check if we have a valid passport!
@@ -105,18 +115,22 @@ namespace AdventOfCode.Year2020
                 }
                 if (validPass)
                 {
-                    validPassports++;
+                    totalValidPassports++;
                     passport = new bool[7];
                 }
             }
 
-            return validPassports.ToString();
+            return totalValidPassports.ToString();
         }
 
-        public bool validHeight(string input) {
-            // a number followed by either cm or in: If cm, the number must be at least 150 and at most 193. If in, the number must be at least 59 and at most 76
+        public bool ValidHeight(string input)
+        {
+            // a number followed by either cm or in, eg 164cm or 60in
+            // If cm, the number must be at least 150 and at most 193. 
+            // If in, the number must be at least 59 and at most 76
             bool validHeight = false;
-            if (input.Length > 2) {
+            if (input.Length > 2)
+            {
                 string last2chars = input[^2..]; // the shiny new RANGE operator
                 int number = int.Parse(input[0..^2]);
                 if (last2chars == "cm" && 150 <= number && number <= 193) { validHeight = true; }
@@ -125,31 +139,33 @@ namespace AdventOfCode.Year2020
             return validHeight;
         }
 
-        public bool validPassportNumber(string input)
+        public bool ValidPassportNumber(string input)
         {
             //a nine-digit number, including leading zeroes
             bool lengthNine = input.Length == 9;
             bool allDigits = true;
-            for (int i = 0; i < input.Length; i++) {
-                if (!char.IsDigit(input[i])){ allDigits = false; break; }
+            for (int i = 0; i < input.Length; i++)
+            {
+                // if there is a non-digit in there, this is not a valid number
+                if (!char.IsDigit(input[i])) { allDigits = false; break; }
             }
             return lengthNine && allDigits;
         }
 
-
-        public bool validHairColor(string input) {
+        public bool ValidHairColor(string input)
+        {
             //a # followed by exactly six characters 0-9 or a-f
 
             var validChars = new HashSet<char>() { 'a', 'b', 'c', 'd', 'e', 'f', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
             bool lengthSeven = input.Length == 7;
             bool hexadecimalCode = true; ;
-            
-                
-                for (int i = 1; i < input.Length; i++)
-                {
-                    if (!validChars.Contains(input[i])) { hexadecimalCode = false; break; }
-                }
-               
+
+            for (int i = 1; i < input.Length; i++)
+            {
+                // if we detect a character which is NOT a digit or a-f, then this is not a valid haircolor.
+                if (!validChars.Contains(input[i])) { hexadecimalCode = false; break; }
+            }
+
             return lengthSeven && hexadecimalCode;
         }
 
