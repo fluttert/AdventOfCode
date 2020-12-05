4a9e0a7fdedb7325f612fd769f8fc8e95bf2d8cc
Ernst Fluttert <e.fluttert@gmail.com>
2020-12-05 15:32:02 +0100

    Day 5 solved and improved

--- AdventOfCode/Year2020/Day05.cs
+++ AdventOfCode/Year2020/Day05.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace AdventOfCode.Year2020
 {
@@ -6,42 +7,86 @@ namespace AdventOfCode.Year2020
     {
         // Puzzle can be found on: https://adventofcode.com/2020/day/5
 
+        /// Generic idea for Day 5 
+        /// If you have spotted that the numbers being asked are encoded in binary
+        /// then this day will be much easier
+        /// BFFFBBF = 1000110 = 70
+
         public string SolvePart1(string input)
         {
-            // read per line, and do NOT remove empty entries (e.g. empty lines)
+            // read per line
             string[] lines = input.Split(Environment.NewLine);
-            return string.Empty;
+            int highestSeatId = int.MinValue;
+            foreach (string line in lines)
+            {
+                int row = BinarySeats(line[0..7]);      // takes the first 7 characters
+                int column = BinarySeats(line[^3..]);   // takes the last 3 characters
+                int seatId = (row * 8) + column;        // calculate seatID
+                highestSeatId = Math.Max(seatId, highestSeatId);
+            }
+            return highestSeatId.ToString();
         }
 
         public string SolvePart2(string input)
         {
             // read per line
             string[] lines = input.Split(Environment.NewLine);
+            int missingSeatId = 0;
+            var seats = new int[lines.Length];
+            for (int i = 0; i < lines.Length; i++)
+            {
+                int row = BinarySeats(lines[i][0..7]);      // takes the first 7 characters
+                int column = BinarySeats(lines[i][^3..]);   // takes the last 3 characters
+                int seatId = (row * 8) + column;        // calculate seatID
+                seats[i] = seatId;
+            }
 
-            return string.Empty;
+            // sort and see which seat is missing
+            Array.Sort(seats);
+            for (int i = 1; i < seats.Length; i++) {
+                if ((seats[i ] - seats[i-1]) == 2) {
+                    missingSeatId = seats[i] - 1;
+                }
+            
+            }
+            return missingSeatId.ToString();
         }
 
         // small pun on BinarySearch
+        // Directly calculate the number without converters
         public int BinarySeats(string code)
         {
-            int min = 1;
-            int max = (int)Math.Pow(2, code.Length); // 2^3 = 8 || 2^7 = 128
+            // the max increment = 2^6 (64), which is then halved every round
+            int seatNumber = 0;
+            int increment = (int)Math.Pow(2, (code.Length - 1));
 
             foreach (char half in code)
             {
-                int difference = (max - min) / 2;
-                if (half == 'F' || half == 'L')
+                // only increment if you have the upperhalf
+                if (half == 'B' || half == 'R')
                 {
-                    // keep lower half
-                    max -= difference;
+                    seatNumber += increment;
                 }
-                else
-                {
-                    min += difference;
-                }
-            }
 
-            return min-1;
+                // and then halve the incremental value
+                increment /= 2;
+            }
+            return seatNumber;
+        }
+
+        // Did not end up using this code -> BinarySeats = better then this 
+        // Idea: Convert the seat-code to a binary string (like 010111)
+        // Then convert binary string to int using built-in converter
+        public int ConvertSeatingToNumber(string code)
+        {
+            var binaryChars = new char[code.Length];
+            for (int i = 0; i < code.Length; i++)
+            {
+                char binchar = '0';
+                if (code[i] == 'B' || code[i] == 'R') { binchar = '1'; }
+                binaryChars[i] = binchar;
+            }
+            return Convert.ToInt32(new string(binaryChars), 2);
         }
 
         public string GetInput()
--- AdventOfCodeTests/Year2020/Day05Tests.cs
+++ AdventOfCodeTests/Year2020/Day05Tests.cs
@@ -10,14 +10,33 @@ namespace AdventOfCodeTests.Year2020
         [Fact]
         public void Part01()
         {
-            Assert.Equal(44, day.BinarySeats("FBFBBFF"));
-            Assert.Equal(5, day.BinarySeats("RLR"));
+            Assert.Equal("357", day.SolvePart1("FBFBBFFRLR"));
         }
 
         [Fact]
-        public void Part02()
+        public void ConvertionToBinary()
         {
-            // empty
+            Assert.Equal(0, day.ConvertSeatingToNumber("F"));
+            Assert.Equal(1, day.ConvertSeatingToNumber("B"));
+            Assert.Equal(0, day.ConvertSeatingToNumber("FF"));
+            Assert.Equal(1, day.ConvertSeatingToNumber("FB"));
+            Assert.Equal(2, day.ConvertSeatingToNumber("BF"));
+            Assert.Equal(3, day.ConvertSeatingToNumber("BB"));
+            Assert.Equal(44, day.ConvertSeatingToNumber("FBFBBFF"));
+            Assert.Equal(5, day.ConvertSeatingToNumber("RLR"));
+        }
+
+        [Fact]
+        public void BinarySeatTest()
+        {
+            Assert.Equal(0, day.BinarySeats("F"));
+            Assert.Equal(1, day.BinarySeats("B"));
+            Assert.Equal(0, day.BinarySeats("FF"));
+            Assert.Equal(1, day.BinarySeats("FB"));
+            Assert.Equal(2, day.BinarySeats("BF"));
+            Assert.Equal(3, day.BinarySeats("BB"));
+            Assert.Equal(44, day.BinarySeats("FBFBBFF"));
+            Assert.Equal(5, day.BinarySeats("RLR"));
         }
     }
 }
