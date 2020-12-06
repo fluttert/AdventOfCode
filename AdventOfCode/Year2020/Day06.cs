0bf4c24dc43e715ca387773810da572a81eec915
Ernst Fluttert <e.fluttert@gmail.com>
2020-12-06 06:48:31 +0100

    Day 6 year 2020

---
+++ AdventOfCode/Year2020/Day06.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AdventOfCode.Year2020
+{
+    public class Day06 : IAoC
+    {
+        // Puzzle can be found on: https://adventofcode.com/2020/day/6
+
+        /// Generic idea for Day 6
+        /// When we are comparing and checking lists; use the datastructure HashSet
+
+        public string SolvePart1(string input)
+        {
+            // read per line
+            string[] lines = input.Split(Environment.NewLine);
+            int sumOfQuestions = 0;
+
+            var letters = new HashSet<char>();
+            for (int i = 0; i < lines.Length; i++)
+            {
+                // add count to sum + reset
+                if (lines[i] == "")
+                {
+                    sumOfQuestions += letters.Count;
+                    letters = new HashSet<char>();
+                }
+                // just count the letters
+                foreach (char c in lines[i])
+                {
+                    if (char.IsLetterOrDigit(c)) { letters.Add(c); }
+                }
+            }
+            return sumOfQuestions.ToString();
+        }
+
+        public string SolvePart2(string input)
+        {
+            // read per line
+            string[] lines = input.Split(Environment.NewLine);
+            int sumOfQuestions = 0;
+            var letters = new HashSet<char>();
+
+
+            for (int i = 0; i < lines.Length; i++)
+            {
+                // add count to sum + reset
+                if (lines[i] == "")
+                {
+                    sumOfQuestions += letters.Count;
+                    letters = new HashSet<char>();
+                    continue;
+                }
+
+                
+                bool firstLine = i == 0 || lines[i - 1] == "";
+                var lettersUpdated = new HashSet<char>();
+                foreach (char c in lines[i])
+                {
+                    // accept everything on the firstline
+                    if (firstLine && char.IsLetterOrDigit(c))
+                    {
+                        lettersUpdated.Add(c);
+                    }
+
+                    // check otherwise if the answer was given before
+                    else
+                    {
+                        if (char.IsLetterOrDigit(c) && letters.Contains(c))
+                        {
+                            lettersUpdated.Add(c);
+                        }
+                    }
+                }
+                // update the list
+                letters = lettersUpdated;
+            }
+
+            return sumOfQuestions.ToString();
+        }
+
+        public string GetInput()
+        {
+            return new Inputs.Year2020.Day06().Input;
+        }
+    }
+}
---
+++ AdventOfCodeTests/Year2020/Day06Tests.cs
@@ -0,0 +1,34 @@
+﻿using AdventOfCode.Year2020;
+using Xunit;
+
+namespace AdventOfCodeTests.Year2020
+{
+    public class Day06Tests
+    {
+        private readonly Day06 day = new Day06();
+
+        [Fact]
+        public void Part02()
+        {
+            string input = @"abc
+
+a
+b
+c
+
+ab
+ac
+
+a
+a
+a
+a
+
+b
+";
+            Assert.Equal("6", day.SolvePart2(input));
+        }
+
+       
+    }
+}
