2d2c5eac5e47cd5db0fb1172b13507773b25b09d
Ernst Fluttert <e.fluttert@gmail.com>
2020-12-10 08:56:36 +0100

    Day10 inprogress

---
+++ AdventOfCode/Year2020/Day10.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AdventOfCode.Year2020
+{
+    public class Day10 : IAoC
+    {
+        // Puzzle can be found on: https://adventofcode.com/2020/day/10
+
+        /// Generic idea for Day 10
+
+        public string SolvePart1(string input)
+        {
+            // read per line and convert to integer array
+            int[] adapters = Array.ConvertAll(input.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries), int.Parse);
+            Array.Sort(adapters); // sorting
+            int diff = adapters[0];
+            int diff1Jolt = diff == 1 ? 1 : 0;
+            int diff3Jolt = diff == 3 ? 1 : 0;
+            for (int i = 1; i < adapters.Length; i++)
+            {
+                diff = adapters[i] - adapters[i - 1];
+                diff1Jolt += diff == 1 ? 1 : 0;
+                diff3Jolt += diff == 3 ? 1 : 0;
+            }
+            diff3Jolt++; // there is ALWAYS a difference of 3 to the max Joltage
+            long sum = diff1Jolt * diff3Jolt;
+            return sum.ToString();
+        }
+
+        // in progress
+        public string SolvePart2(string input)
+        {
+            // read per line
+            int[] adapters = Array.ConvertAll(input.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries), int.Parse);
+            Array.Sort(adapters); // sorting
+            var adaptersAvailable = new HashSet<int>(adapters);
+            var queue = new Queue<int>();
+            queue.Enqueue(0); // starting point
+            int maxJoltage = adapters[^1] + 3;
+            long arragements = 0;
+            while (queue.Count > 0) {
+                int curJoltage = queue.Dequeue();
+                if (adaptersAvailable.Contains(curJoltage + 1)) { arragements++; }
+            }
+            return arragements.ToString();
+        }
+
+        public string GetInput()
+        {
+            return new Inputs.Year2020.Day10().Input;
+        }
+    }
+}
---
+++ AdventOfCodeTests/Year2020/Day10Tests.cs
@@ -0,0 +1,71 @@
+﻿using AdventOfCode.Year2020;
+using Xunit;
+
+namespace AdventOfCodeTests.Year2020
+{
+    public class Day10Tests
+    {
+        private readonly Day10 day = new Day10();
+
+        [Fact]
+        public void Part01Test1()
+        {
+            string input = @"16
+10
+15
+5
+1
+11
+7
+19
+6
+12
+4";
+            Assert.Equal("35", day.SolvePart1(input));
+        }
+
+        [Fact]
+        public void Part01Test2()
+        {
+            string input = @"28
+33
+18
+42
+31
+14
+46
+20
+48
+47
+24
+23
+49
+45
+19
+38
+39
+11
+1
+32
+25
+35
+8
+17
+7
+9
+4
+2
+34
+10
+3";
+            Assert.Equal("220", day.SolvePart1(input));
+        }
+
+        [Fact]
+        public void Part02Test()
+        {
+            // no tests
+        }
+       
+    }
+}
