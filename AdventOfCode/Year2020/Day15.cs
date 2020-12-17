6b3c8eb2d750603710dd9ff3e5ed0253f0d8a31b
Ernst Fluttert <e.fluttert@gmail.com>
2020-12-17 07:54:36 +0100

    Day15 solved

---
+++ AdventOfCode/Year2020/Day15.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AdventOfCode.Year2020
+{
+    public class Day15 : IAoC
+    {
+        // Puzzle can be found on: https://adventofcode.com/2020/day/15
+
+        /// Generic idea for Day 15
+        ///
+        public string SolvePart1(string input)
+        {
+
+            return MemoryGame(input, 2020).ToString();
+        }
+
+        // in progress
+        public string SolvePart2(string input)
+        {
+            // read per lines
+            return MemoryGame(input, 30000000).ToString();
+        }
+
+        public int MemoryGame(string input, int turnsBeforeExit)
+        {
+
+            int[] startNumbers = Array.ConvertAll(input.Split(','), int.Parse);
+            var dict = new Dictionary<int, int>(); // spokennumber - lastRound 
+            int turn = 1;
+            int lastNumber = -1;
+
+            // add the initials
+            foreach (var number in startNumbers)
+            {
+                dict.Add(number, turn);
+                turn++;
+                lastNumber = number;
+            }
+
+            while (turn <= turnsBeforeExit)
+            {
+                // consider last number
+                int nextNumber = -1;
+
+
+                // If it was spoken for the first time, tell zero!
+                if ((turn - 1 == dict[lastNumber]))
+                {
+                    nextNumber = 0;
+                }
+
+                // otherwise is was spoken before
+                else
+                {
+                    nextNumber = turn - 1 - dict[lastNumber];
+                    dict[lastNumber] = turn - 1;
+                }
+
+
+
+                // add to dictionary if necessary
+                if (dict.ContainsKey(nextNumber) is false) { dict.Add(nextNumber, turn); }
+                lastNumber = nextNumber;
+                turn++;
+            }
+
+
+            return lastNumber;
+        } 
+       
+
+        public string GetInput() => @"6,4,12,1,20,0,16";
+    }
+}
---
+++ AdventOfCodeTests/Year2020/Day15Tests.cs
@@ -0,0 +1,24 @@
+﻿using AdventOfCode.Year2020;
+using Xunit;
+
+namespace AdventOfCodeTests.Year2020
+{
+    public class Day15Tests
+    {
+        private readonly Day15 day = new Day15();
+
+        [Fact]
+        public void Part01Test1()
+        {
+            string input = @"1,3,2";
+            Assert.Equal("1", day.SolvePart1(input));
+        }
+
+        [Fact]
+        public void Part01Test2()
+        {
+            string input = @"0,3,6";
+            Assert.Equal(0, day.MemoryGame(input,10));
+        }
+    }
+}
