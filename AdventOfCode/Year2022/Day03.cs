a7e09f4d20fb5d4ce0fb75dcdbf2282ed2054150
Ernst Fluttert <e.fluttert@gmail.com>
2022-12-03 23:32:26 +0100

    Format Day 3

--- AdventOfCode/Year2022/Day03.cs
+++ AdventOfCode/Year2022/Day03.cs
@@ -36,23 +36,26 @@ namespace AdventOfCode.Year2022
         {
             int sum = 0;
             string[] lines = input.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries);
-            for (int i = 0; i < lines.Length; i+=3) { 
-                HashSet<char> rucksack1 = new();
+            for (int i = 0; i < lines.Length; i += 3)
+            {
+                HashSet<char> rucksack1 = new()
                 foreach (char rs1 in lines[i]) { rucksack1.Add(rs1); }
                 HashSet<char> rucksack2 = new();
-                foreach (char rs2 in lines[i + 1]) { 
-                    if (rucksack1.Contains(rs2)) { rucksack2.Add(rs2); } 
+                foreach (char rs2 in lines[i + 1])
+                {
+                    if (rucksack1.Contains(rs2)) { rucksack2.Add(rs2); }
                 }
                 // no hashset needed for last, should only be 1
-                foreach (char rs3 in lines[i + 2]) {
-                    if (rucksack2.Contains(rs3)) {
+                foreach (char rs3 in lines[i + 2])
+                {
+                    if (rucksack2.Contains(rs3))
+                    {
                         sum += char.IsUpper(rs3) ? (rs3 - 'A' + 27) : (rs3 - 'a' + 1);
                         break;
                     }
                 }
-            
             }
-            return ""+sum;
+            return "" + sum;
         }
 
         public string GetInput()
