ba6f61f248c54cabb3adadb61cf4853572c4fa50
Ernst Fluttert <e.fluttert@gmail.com>
2022-12-07 17:06:44 +0100

    Improved Day 7

--- AdventOfCode/Year2022/Day07.cs
+++ AdventOfCode/Year2022/Day07.cs
@@ -7,12 +7,14 @@ namespace AdventOfCode.Year2022
     {
         // Puzzle can be found on: https://adventofcode.com/2022/day/7
 
+        private Dictionary<string, Directory> tree = new();
+        private Directory root;
+
         public string SolvePart1(string input)
         {
             string[] lines = input.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries);
-            Dictionary<string, Directory> tree = new();
-            Directory root = new Directory { Name = "/" };
-            tree.Add(root.FullName(), root);
+            root = new Directory { Name = "/" };
+            tree.Add(root.UniqueName, root);
             Directory current = null;
             for (int i = 0; i < lines.Length; i++)
             {
@@ -20,30 +22,12 @@ namespace AdventOfCode.Year2022
                 // command
                 if (parts[0] == "$")
                 {
-                    // go to root
-                    if (parts[1] == "cd" && parts[2] == "/")
-                    {
-                        current = root;
-                        continue;
-                    }
-                    // go up
-                    if (parts[1] == "cd" && parts[2] == "..")
-                    {
-                        current = current.Parent;
-                        continue;
-                    }
-                    // ignore LS
-                    if (parts[1] == "ls") { continue; }
+                    if (parts[1] == "cd" && parts[2] == "/") { current = root; continue; } // go to Root
+                    if (parts[1] == "cd" && parts[2] == "..") { current = current.Parent; continue; } // go to Parent
+                    if (parts[1] == "ls") { continue; } // ignore LS
 
-                    // go to child
-                    foreach (Directory child in current.Children)
-                    {
-                        if (child.Name == parts[2])
-                        {
-                            current = child;
-                            break;
-                        }
-                    }
+                    // else go to child
+                    current = current.SubDirectories[parts[2]];
                     continue;
                 }
 
@@ -51,20 +35,46 @@ namespace AdventOfCode.Year2022
                 if (parts[0] == "dir")
                 {
                     Directory tmp = new Directory { Name = parts[1], Parent = current };
-                    tree.Add(tmp.FullName(), tmp);
-                    current.Children.Add(tmp);
+                    tree.Add(tmp.UniqueName, tmp);
+                    current.SubDirectories.Add(tmp.Name, tmp);
                     continue;
                 }
 
                 // no command -> handle Files second
-                current.AddFile(parts[1], parts[0]);
+                current.AddFile(parts[0]);
+            }
+
+            // calc all
+            Queue<Directory> queue = new();
+            HashSet<string> done = new();
+            // add all directories without children
+            foreach (var dir in tree.Values) { if (dir.SubDirectories.Count == 0) { queue.Enqueue(dir); } }
+            // dequeue and add parents
+            while (queue.Count > 0)
+            {
+                var d = queue.Dequeue();
+                if (done.Contains(d.Name) || d.Name == root.Name) { continue; }
+                bool allComplete = true;
+                foreach (var dir in d.SubDirectories.Values)
+                {
+                    if (!done.Contains(dir.UniqueName))
+                    {
+                        allComplete = false;
+                        queue.Enqueue(dir);
+                    }
+                }
+                if (allComplete is false) { queue.Enqueue(d); continue; }
+
+                // all children done
+                d.Parent.Size += d.Size;
+                queue.Enqueue(d.Parent);
+                done.Add(d.UniqueName);
             }
 
             long sumOfSmallDirs = 0;
             foreach (Directory dir in tree.Values)
             {
-                long dirSize = dir.DirectoryFileSize();
-                if (dirSize <= 100_000) { sumOfSmallDirs += dirSize; }
+                if (dir.Size <= 100_000) { sumOfSmallDirs += dir.Size; }
             }
 
             return "" + sumOfSmallDirs;
@@ -72,66 +82,14 @@ namespace AdventOfCode.Year2022
 
         public string SolvePart2(string input)
         {
-            string[] lines = input.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries);
-            Dictionary<string, Directory> tree = new();
-            Directory root = new Directory { Name = "/" };
-            tree.Add(root.FullName(), root);
-            Directory current = null;
-            for (int i = 0; i < lines.Length; i++)
-            {
-                string[] parts = lines[i].Split(' ');
-                // command
-                if (parts[0] == "$")
-                {
-                    // go up
-                    if (parts[1] == "cd" && parts[2] == "..")
-                    {
-                        current = current.Parent;
-                        continue;
-                    }
-                    if (parts[1] == "ls") { continue; }
-
-                    // go to root
-                    if (parts[1] == "cd" && parts[2] == "/")
-                    {
-                        current = root;
-                        continue;
-                    }
-                    // go to child
-                    foreach (Directory child in current.Children)
-                    {
-                        if (child.Name == parts[2])
-                        {
-                            current = child;
-                            break;
-                        }
-                    }
-                    continue;
-                }
-
-                // no command -> handle Directory first
-                if (parts[0] == "dir")
-                {
-                    Directory tmp = new Directory { Name = parts[1], Parent = current };
-                    tree.Add(tmp.FullName(), tmp);
-                    current.Children.Add(tmp);
-                    continue;
-                }
-
-                // no command -> handle Files second
-                current.AddFile(parts[1], parts[0]);
-            }
-
-            long totalSpace = 70_000_000;
-            long unusedSpaceRequired = 30_000_000;
-            long unusedSpace = totalSpace - root.DirectoryFileSize();
+            long totalSpace = 70_000_000, unusedSpaceRequired = 30_000_000;
+            long unusedSpace = totalSpace - root.Size;
             long closest = long.MaxValue;
             foreach (Directory dir in tree.Values)
             {
-                long dirSize = dir.DirectoryFileSize();
-                if (unusedSpace + dirSize >= unusedSpaceRequired && dirSize < closest)
+                if (unusedSpace + dir.Size >= unusedSpaceRequired && dir.Size < closest)
                 {
-                    closest = dirSize;
+                    closest = dir.Size;
                 }
             }
 
@@ -144,49 +102,18 @@ namespace AdventOfCode.Year2022
         }
     }
 
+    // Helper class
     public class Directory
     {
         public string Name;
-        public Directory Parent = null;
-        public List<Directory> Children = new();
-        private List<(string name, long size)> files = new();
+        public string UniqueName = Guid.NewGuid().ToString();
+        public long Size = 0;
+        public Directory Parent = null; // root has Parent = NULL
+        public Dictionary<string, Directory> SubDirectories = new();
 
-        public string FullName()
+        public void AddFile(string size)
         {
-            string name = Name;
-            Directory p = Parent;
-            while (p != null)
-            {
-                name = p.Name + name;
-                p = p.Parent;
-            }
-            return name;
-        }
-
-        public bool AddFile(string filename, string size)
-        {
-            return AddFile(filename, long.Parse(size));
-        }
-
-        public bool AddFile(string filename, long size)
-        {
-            files.Add((filename, size));
-            return true;
-        }
-
-        public long DirectoryFileSize()
-        {
-            long total = 0;
-            foreach (var (name, size) in files)
-            {
-                total += size;
-            }
-            foreach (Directory dir in Children)
-            {
-                total += dir.DirectoryFileSize();
-            }
-
-            return total;
+            this.Size += long.Parse(size);
         }
     }
 }
