4d19809c32c00f578d2b89c9f79a81505f292c9e
Fluttert <e.fluttert@gmail.com>
2024-04-11 21:22:43 +0200

    Add day 13

---
+++ AdventOfCode/Year2023/Day12.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventOfCode.Year2023;
+
+public class Day12 : IAoC
+{
+    // Puzzle can be found on: https://adventofcode.com/2023/day/12
+    public string SolvePart1(string input)
+    {
+        return "";
+    }
+
+    public string SolvePart2(string input)
+    {
+
+
+        return "";
+    }
+
+    public string GetInput()
+    {
+        //return new Inputs.Year2023.Day12Input().Input;
+        return """
+testdata
+""";
+    }
+}
